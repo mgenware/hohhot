2656292a395a1011b386f0048e0d7ff34055955b
Mgen <m@mgenware.com>
2018-04-05 16:19:36 +0800

    Add support for parsing declaration with type

--- lib/core/VariableStatement.ts
+++ lib/core/VariableStatement.ts
@@ -1,7 +1,24 @@
 import * as ts from 'typescript';
 import { Node, NodeType } from '../base';
 
+export class TypeReferenceNode {
+  static fromTypeReferenceNode(node: ts.TypeReferenceNode) {
+    if (ts.isIdentifier(node.typeName)) {
+      const id = node.typeName as ts.Identifier;
+      const name = ts.idText(id);
+      return new TypeReferenceNode(name);
+    }
+    throw new Error(`node.typeName is not an identifier`);
+  }
+
+  constructor(
+    public name: string,
+  ) {}
+}
+
 export class VariableDeclaration {
+  type?: TypeReferenceNode;
+
   constructor(
     public id: string,
   ) {}
@@ -19,12 +36,19 @@ export class VariableStatement extends Node {
   }
 }
 
-function parseVariableDeclaration(dec: ts.VariableDeclaration): VariableDeclaration {
-  if (dec.name.kind === ts.SyntaxKind.Identifier) {
-    const decID = dec.name as ts.Identifier;
-    return new VariableDeclaration(ts.idText(decID));
+function parseVariableDeclaration(rawDec: ts.VariableDeclaration): VariableDeclaration {
+  if (rawDec.name.kind === ts.SyntaxKind.Identifier) {
+    // Name
+    const name = rawDec.name as ts.Identifier;
+    const dec = new VariableDeclaration(ts.idText(name));
+
+    // Type
+    if (rawDec.type && ts.isTypeReferenceNode(rawDec.type)) {
+      dec.type = TypeReferenceNode.fromTypeReferenceNode(rawDec.type);
+    }
+    return dec;
   }
-  throw new Error(`Unsupported VariableDeclaration.name.kind: ${dec.name.kind}`);
+  throw new Error(`Unsupported VariableDeclaration.name.kind: ${rawDec.name.kind}`);
 }
 
 export function parseVariableStatement(node: ts.VariableStatement): VariableStatement {
--- tests/core/VariableStatement.test.ts
+++ tests/core/VariableStatement.test.ts
@@ -1,5 +1,5 @@
 import { buildNode, NodeType } from '../base';
-import { VariableStatement } from '../../lib/core/VariableStatement';
+import { VariableStatement, TypeReferenceNode } from '../../lib/core/VariableStatement';
 
 test('Single var', () => {
   const node = buildNode('var xy;') as VariableStatement;
@@ -10,6 +10,7 @@ test('Single var', () => {
 
   const dec = decList[0];
   expect(dec.id).toBe('xy');
+  expect(dec.type).toBeUndefined();
 });
 
 test('Multiple vars', () => {
@@ -22,5 +23,30 @@ test('Multiple vars', () => {
   const values = ['a', 'b', 'c'];
   for (let i = 0; i < decList.length; i++) {
     expect(decList[i].id).toBe(values[i]);
+    expect(decList[i].type).toBeUndefined();
+  }
+});
+
+test('Multiple vars with types', () => {
+  const node = buildNode('var a:Type1,b,c:Type2;') as VariableStatement;
+  expect(node.type).toBe(NodeType.VariableStatement);
+
+  const decList = node.declarationList;
+  expect(decList.length).toBe(3);
+
+  const values = ['a', 'b', 'c'];
+  const types = ['Type1', undefined, 'Type2'];
+
+  for (let i = 0; i < decList.length; i++) {
+    const dec = decList[i];
+    expect(dec.id).toBe(values[i]);
+
+    const typeName = types[i];
+    if (typeName) {
+      const typeNode = dec.type as TypeReferenceNode;
+      expect(typeNode.name).toBe(typeName);
+    } else {
+      expect(dec.type).toBeUndefined();
+    }
   }
 });
