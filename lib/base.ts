4a025ae0752da7d98b77c47ddc6cb6ddba5146fd
Mgen <m@mgenware.com>
2018-04-05 15:22:53 +0800

    Add ast function to transform src string to ts.Statement

--- lib/base.ts
+++ lib/base.ts
@@ -8,12 +8,17 @@ export class Node {
   nodeType(): NodeType {
     throw new Error('Not implemented yet');
   }
+
+  get type(): NodeType {
+    return this.nodeType();
+  }
 }
 
-export class Parser {
-  src: ts.Node;
+export function ast(src: string): ts.Statement[] {
+  const srcFile = ts.createSourceFile('t', src, ts.ScriptTarget.Latest);
+  return [...srcFile.statements];
+}
 
-  parse(): Node {
-    throw new Error('Not implemented yet');
-  }
+export function astNode(src: string): ts.Statement {
+  return ast(src)[0];
 }
