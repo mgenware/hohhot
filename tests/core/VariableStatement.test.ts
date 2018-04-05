93b7867be4f3172c824c084e88ddc90eeb79a4ed
Mgen <m@mgenware.com>
2018-04-05 15:27:13 +0800

    Update test for single var dec parsing

--- tests/core/VariableStatement.test.ts
+++ tests/core/VariableStatement.test.ts
@@ -1,7 +1,13 @@
 import { buildNode, NodeType } from '../base';
 import { VariableStatement } from '../../lib/core/VariableStatement';
 
-test('VariableStatement.test', () => {
-  const node = buildNode('var i;') as VariableStatement;
+test('Single var', () => {
+  const node = buildNode('var xy;') as VariableStatement;
   expect(node.type).toBe(NodeType.VariableStatement);
+
+  const decList = node.declarationList;
+  expect(decList.length).toBe(1);
+
+  const dec = decList[0];
+  expect(dec.id).toBe('xy');
 });
