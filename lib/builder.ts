ed6b189085e5776e302f9eca8d0d42beca9d1a6b
Mgen <m@mgenware.com>
2018-04-05 16:22:44 +0800

    Move parsing code to fromNode function

--- lib/builder.ts
+++ lib/builder.ts
@@ -1,11 +1,11 @@
 import { Node } from './base';
 import * as ts from 'typescript';
-import { parseVariableStatement } from './core/VariableStatement';
+import { VariableStatement } from './core/VariableStatement';
 
 export default class Builder {
   static buildNode(node: ts.Node): Node {
     switch (node.kind) {
-      case ts.SyntaxKind.VariableStatement: return parseVariableStatement(node as ts.VariableStatement);
+      case ts.SyntaxKind.VariableStatement: return VariableStatement.fromNode(node as ts.VariableStatement);
     }
 
     throw new Error(`node.type not supported: ${node.kind}`);
--- lib/core/VariableStatement.ts
+++ lib/core/VariableStatement.ts
@@ -2,7 +2,7 @@ import * as ts from 'typescript';
 import { Node, NodeType } from '../base';
 
 export class TypeReferenceNode {
-  static fromTypeReferenceNode(node: ts.TypeReferenceNode) {
+  static fromNode(node: ts.TypeReferenceNode) {
     if (ts.isIdentifier(node.typeName)) {
       const id = node.typeName as ts.Identifier;
       const name = ts.idText(id);
@@ -17,6 +17,21 @@ export class TypeReferenceNode {
 }
 
 export class VariableDeclaration {
+  static fromNode(rawDec: ts.VariableDeclaration): VariableDeclaration {
+    if (rawDec.name.kind === ts.SyntaxKind.Identifier) {
+      // Name
+      const name = rawDec.name as ts.Identifier;
+      const dec = new VariableDeclaration(ts.idText(name));
+
+      // Type
+      if (rawDec.type && ts.isTypeReferenceNode(rawDec.type)) {
+        dec.type = TypeReferenceNode.fromNode(rawDec.type);
+      }
+      return dec;
+    }
+    throw new Error(`Unsupported VariableDeclaration.name.kind: ${rawDec.name.kind}`);
+  }
+
   type?: TypeReferenceNode;
 
   constructor(
@@ -25,6 +40,15 @@ export class VariableDeclaration {
 }
 
 export class VariableStatement extends Node {
+  static fromNode(node: ts.VariableStatement): VariableStatement {
+    const statement = node as ts.VariableStatement;
+    const decList = statement.declarationList;
+    const parsedDecList = decList.declarations.map((dec) => {
+      return VariableDeclaration.fromNode(dec);
+    });
+    return new VariableStatement(parsedDecList);
+  }
+
   constructor(
     public declarationList: VariableDeclaration[],
   ) {
@@ -34,28 +58,4 @@ export class VariableStatement extends Node {
   nodeType(): NodeType {
     return NodeType.VariableStatement;
   }
-}
-
-function parseVariableDeclaration(rawDec: ts.VariableDeclaration): VariableDeclaration {
-  if (rawDec.name.kind === ts.SyntaxKind.Identifier) {
-    // Name
-    const name = rawDec.name as ts.Identifier;
-    const dec = new VariableDeclaration(ts.idText(name));
-
-    // Type
-    if (rawDec.type && ts.isTypeReferenceNode(rawDec.type)) {
-      dec.type = TypeReferenceNode.fromTypeReferenceNode(rawDec.type);
-    }
-    return dec;
-  }
-  throw new Error(`Unsupported VariableDeclaration.name.kind: ${rawDec.name.kind}`);
-}
-
-export function parseVariableStatement(node: ts.VariableStatement): VariableStatement {
-  const statement = node as ts.VariableStatement;
-  const decList = statement.declarationList;
-  const parsedDecList = decList.declarations.map((dec) => {
-    return parseVariableDeclaration(dec);
-  });
-  return new VariableStatement(parsedDecList);
 }
