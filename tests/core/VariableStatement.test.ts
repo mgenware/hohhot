8bc764f0fc729516d75bc20b9deb22de1f4aee3b
Mgen <m@mgenware.com>
2018-04-05 15:43:16 +0800

    Add test for multiple var decs

--- tests/core/VariableStatement.test.ts
+++ tests/core/VariableStatement.test.ts
@@ -11,3 +11,16 @@ test('Single var', () => {
   const dec = decList[0];
   expect(dec.id).toBe('xy');
 });
+
+test('Multiple vars', () => {
+  const node = buildNode('var a,b,c;') as VariableStatement;
+  expect(node.type).toBe(NodeType.VariableStatement);
+
+  const decList = node.declarationList;
+  expect(decList.length).toBe(3);
+
+  const values = ['a', 'b', 'c'];
+  for (let i = 0; i < decList.length; i++) {
+    expect(decList[i].id).toBe(values[i]);
+  }
+});
